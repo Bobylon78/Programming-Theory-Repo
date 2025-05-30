27e631fda1c99071a2b3b388be800293900b22dd
Bobylon <Bobylon78@users.noreply.github.com>
2025-05-30 15:21:29 +0200

    Sound
    -Ajout des diffèrent Sound du projet

--- Assets/Scripts/Script  manager/MyScaneManager.cs
+++ Assets/Scripts/Script  manager/MyScaneManager.cs
@@ -5,6 +5,12 @@ public class MyScaneManager : MonoBehaviour
 {
     public static MyScaneManager Instance;
 
+    public AudioSource audioSource;
+    public AudioClip introClip;
+    public AudioClip introLoopClip;
+    public AudioClip simulationLoopClip;
+    private bool clipJouer = false;
+
     private void Awake()
     {
         if (Instance == null)
@@ -18,8 +24,42 @@ public class MyScaneManager : MonoBehaviour
         }
 
     }
+    public void Start()
+    {
+        if(SceneManager.GetActiveScene().name == "Menue_Start")
+        {
+            JouerAudioIntro();
+        }
+    }
     public void Lancement()
     {
         SceneManager.LoadScene("Simulation");
+        if(simulationLoopClip != null)
+        {
+            audioSource.clip = simulationLoopClip;
+            audioSource.Play();
+        }
+    }
+    void JouerAudioIntro()
+    {
+        if (audioSource != null && introClip != null)
+        {
+            audioSource.loop = false;
+            audioSource.clip = introClip;
+            audioSource.Play();
+            StartCoroutine(FinClipIntro());
+        }
+    }
+    System.Collections.IEnumerator FinClipIntro()
+    {
+        yield return new WaitForSeconds(introClip.length);
+
+        if(introLoopClip !=null)
+        {
+            audioSource.clip = introLoopClip;
+            audioSource.loop = true;
+            audioSource.Play();
+            clipJouer = true;
+        }
     }
 }
